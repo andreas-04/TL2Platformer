527e5ff89784e4750cb2eb21b07c97db56ffaa09
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-08-29 19:25:43 -0700

    add score update function
    - update score in GameManager when a coin is collected

--- Assets/Lance/CoinScript.cs
+++ Assets/Lance/CoinScript.cs
@@ -1,20 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     public int coinValue = 1; // The value of the coin
-    public GameObject gameManager;
+    public GameObject gameManagerObject; 
+    public GameManager gameManager; 
+
+    void Start()
+    {
+        // Find the GameObject with the tag "GameController"
+        gameManagerObject = GameObject.FindWithTag("GameController");
+
+        // Get the GameManager component from the GameObject
+        if (gameManagerObject != null)
+        {
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
+        else
+        {
+            Debug.LogError("GameManager GameObject not found with tag 'GameController'.");
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the object that collided with the coin is the player
         if (other.CompareTag("Player"))
         {
-            // Add coin value to player's score
-            //gameManager.addScore(coinValue);
+            // Ensure the gameManager reference is not null
+            if (gameManager != null)
+            {
+                // Add coin value to player's score
+                gameManager.AddCoin(coinValue);
 
-            // make the coin dissapear
-            gameObject.SetActive(false);
+                // Make the coin disappear
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("GameManager reference is missing.");
+            }
         }
     }
 }
